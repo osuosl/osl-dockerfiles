888aa8a6bd46b9fd2a3529b5ee5482332c7b1c89
Evan Tschuy <tschuy@osuosl.org>
2015-07-08 14:59:13 -0700

    Finish python_webapp_spec specs

--- spec/python_webapp_spec.rb
+++ spec/python_webapp_spec.rb
@@ -13,14 +13,38 @@ describe 'Docker configuration' do
   end
 end
 
-describe package('nginx') do
-  it { should be_installed }
+%w(gcc python-devel python-setuptools libffi-devel nginx).each do |pkg|
+  describe package(pkg) do
+    it { should be_installed }
+  end
 end
 
 describe command('pip') do
   its(:exit_status) { should eq 0 }
 end
 
+%w(gunicorn supervisor).each do |pkg|
+  describe package(pkg) do
+    it { should be_installed.by('pip') }
+  end
+end
+
 describe command('cat /etc/redhat-release') do
   its(:stdout) { should contain 'CentOS release 6' }
+end
+
+describe file('/etc/supervisord.conf') do
+  its(:content) { should include('files = /etc/supervisor.d/*.conf') }
+end
+
+describe file('/opt/app/conf/gunicorn_config.py') do
+  it { should exist }
+end
+
+describe file('/etc/nginx/conf.d/app.conf') do
+  it { should exist }
+end
+
+describe file('/etc/nginx/conf.d/default.conf') do
+  it { should_not exist }
 end

e6b586ed420e34e6bc1dbd428d786d63a80088fe
Lance Albertson <lance@osuosl.org>
2018-04-19 10:51:16 -0700

    Update Ubuntu ppc64le jenkins images / add ccache
    This provides the following updates:
    
    - Adds ccache with a config which points to a volume we'll setup on the
      server-side
    - Do a apt-get upgrade so we pull the latest for all
    - Create a VERSION argument which points to the tag of the source image
    - Add 18.04 for ubuntu-ppc64le
    - Cleanup format to match ubuntu Dockerfiles
    - Create build.sh and push.sh scripts to automate building of all tags using the
      new VERSION argument
    
    For the CUDA version:
    
    - Remove all the subfolders which are no longer needed since we can use the
      VERSION argument now

---
+++ ubuntu-ppc64le-cuda/build.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -ex
+docker build -t ubuntu-ppc64le-cuda:8.0 --build-arg VERSION=8.0-devel-ubuntu16.04 .
+docker build -t ubuntu-ppc64le-cuda:9.0 --build-arg VERSION=9.0-devel-ubuntu16.04 .
+docker build -t ubuntu-ppc64le-cuda:9.1 --build-arg VERSION=9.1-devel-ubuntu16.04 .
+docker tag ubuntu-ppc64le-cuda:9.1 ubuntu-ppc64le-cuda:latest
+docker build -t ubuntu-ppc64le-cuda:8.0-cudnn6 --build-arg VERSION=8.0-cudnn6-devel-ubuntu16.04 .
+docker build -t ubuntu-ppc64le-cuda:8.0-cudnn7 --build-arg VERSION=8.0-cudnn7-devel-ubuntu16.04 .
+docker build -t ubuntu-ppc64le-cuda:9.0-cudnn7 --build-arg VERSION=9.0-cudnn7-devel-ubuntu16.04 .
+docker build -t ubuntu-ppc64le-cuda:9.1-cudnn7 --build-arg VERSION=9.1-cudnn7-devel-ubuntu16.04 .
---
+++ ubuntu-ppc64le/build.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+set -ex
+docker build -t osuosl/ubuntu-ppc64le:16.04 --build-arg VERSION=16.04 .
+docker tag osuosl/ubuntu-ppc64le:16.04 osuosl/ubuntu-ppc64le:latest
+docker build -t osuosl/ubuntu-ppc64le:18.04 --build-arg VERSION=18.04 .

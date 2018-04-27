b72e89729319d8bc286b7615a6d03a341079a9a9
Lance Albertson <lance@osuosl.org>
2018-04-27 13:26:00 -0700

    Fix docker path

--- ubuntu-ppc64le-cuda/push.sh
+++ ubuntu-ppc64le-cuda/push.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 set -ex
-docker push ubuntu-ppc64le-cuda:8.0
-docker push ubuntu-ppc64le-cuda:9.0
-docker push ubuntu-ppc64le-cuda:9.1
-docker push ubuntu-ppc64le-cuda:latest
-docker push ubuntu-ppc64le-cuda:8.0-cudnn6
-docker push ubuntu-ppc64le-cuda:8.0-cudnn7
-docker push ubuntu-ppc64le-cuda:9.0-cudnn7
-docker push ubuntu-ppc64le-cuda:9.1-cudnn7
+docker push osuosl/ubuntu-ppc64le-cuda:8.0
+docker push osuosl/ubuntu-ppc64le-cuda:9.0
+docker push osuosl/ubuntu-ppc64le-cuda:9.1
+docker push osuosl/ubuntu-ppc64le-cuda:latest
+docker push osuosl/ubuntu-ppc64le-cuda:8.0-cudnn6
+docker push osuosl/ubuntu-ppc64le-cuda:8.0-cudnn7
+docker push osuosl/ubuntu-ppc64le-cuda:9.0-cudnn7
+docker push osuosl/ubuntu-ppc64le-cuda:9.1-cudnn7

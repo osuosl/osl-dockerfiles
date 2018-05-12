ba2f66a1266000cf2ef4bc1715f543bac40f6c29
Lance Albertson <lance@osuosl.org>
2018-05-12 10:23:11 -0700

    Add MySQL-python and setup mysqladmin

--- dobc-centos/mysql.sh
+++ dobc-centos/mysql.sh
@@ -1,1 +1,2 @@
 alias mysql="mysql -h\"$MYSQL_PORT_3306_TCP_ADDR\" -P\"$MYSQL_PORT_3306_TCP_PORT\" -uroot -p\"$MYSQL_ENV_MYSQL_ROOT_PASSWORD\""
+alias mysqladmin="mysqladmin -h\"$MYSQL_PORT_3306_TCP_ADDR\" -P\"$MYSQL_PORT_3306_TCP_PORT\" -uroot -p\"$MYSQL_ENV_MYSQL_ROOT_PASSWORD\""

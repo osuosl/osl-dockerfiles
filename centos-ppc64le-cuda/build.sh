ac0930dcb69bd661cba2f848f35f6ea2e5e44144
Lance Albertson <lance@osuosl.org>
2018-10-16 09:53:41 -0700

    Set 9.2 as latest tag

--- centos-ppc64le-cuda/build.sh
+++ centos-ppc64le-cuda/build.sh
@@ -4,7 +4,7 @@ docker build -t osuosl/centos-ppc64le-cuda:8.0 --build-arg VERSION=8.0-devel-cen
 docker build -t osuosl/centos-ppc64le-cuda:9.0 --build-arg VERSION=9.0-devel-centos7 .
 docker build -t osuosl/centos-ppc64le-cuda:9.1 --build-arg VERSION=9.1-devel-centos7 .
 docker build -t osuosl/centos-ppc64le-cuda:9.2 --build-arg VERSION=9.2-devel-centos7 .
-docker tag osuosl/centos-ppc64le-cuda:9.1 osuosl/centos-ppc64le-cuda:latest
+docker tag osuosl/centos-ppc64le-cuda:9.2 osuosl/centos-ppc64le-cuda:latest
 docker build -t osuosl/centos-ppc64le-cuda:8.0-cudnn6 --build-arg VERSION=8.0-cudnn6-devel-centos7 .
 docker build -t osuosl/centos-ppc64le-cuda:8.0-cudnn7 --build-arg VERSION=8.0-cudnn7-devel-centos7 .
 docker build -t osuosl/centos-ppc64le-cuda:9.0-cudnn7 --build-arg VERSION=9.0-cudnn7-devel-centos7 .
--- ubuntu-ppc64le-cuda/build.sh
+++ ubuntu-ppc64le-cuda/build.sh
@@ -4,7 +4,7 @@ docker build -t osuosl/ubuntu-ppc64le-cuda:8.0 --build-arg VERSION=8.0-devel-ubu
 docker build -t osuosl/ubuntu-ppc64le-cuda:9.0 --build-arg VERSION=9.0-devel-ubuntu16.04 .
 docker build -t osuosl/ubuntu-ppc64le-cuda:9.1 --build-arg VERSION=9.1-devel-ubuntu16.04 .
 docker build -t osuosl/ubuntu-ppc64le-cuda:9.2 --build-arg VERSION=9.2-devel-ubuntu16.04 .
-docker tag osuosl/ubuntu-ppc64le-cuda:9.1 osuosl/ubuntu-ppc64le-cuda:latest
+docker tag osuosl/ubuntu-ppc64le-cuda:9.2 osuosl/ubuntu-ppc64le-cuda:latest
 docker build -t osuosl/ubuntu-ppc64le-cuda:8.0-cudnn6 --build-arg VERSION=8.0-cudnn6-devel-ubuntu16.04 .
 docker build -t osuosl/ubuntu-ppc64le-cuda:8.0-cudnn7 --build-arg VERSION=8.0-cudnn7-devel-ubuntu16.04 .
 docker build -t osuosl/ubuntu-ppc64le-cuda:9.0-cudnn7 --build-arg VERSION=9.0-cudnn7-devel-ubuntu16.04 .

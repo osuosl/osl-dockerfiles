5eba2a48b5c368fadbb00d4df711978c27bbb43b
Lance Albertson <lance@osuosl.org>
2018-10-16 09:51:30 -0700

    Add CUDA 9.2 images

--- centos-ppc64le-cuda/build.sh
+++ centos-ppc64le-cuda/build.sh
@@ -3,8 +3,10 @@ set -ex
 docker build -t osuosl/centos-ppc64le-cuda:8.0 --build-arg VERSION=8.0-devel-centos7 .
 docker build -t osuosl/centos-ppc64le-cuda:9.0 --build-arg VERSION=9.0-devel-centos7 .
 docker build -t osuosl/centos-ppc64le-cuda:9.1 --build-arg VERSION=9.1-devel-centos7 .
+docker build -t osuosl/centos-ppc64le-cuda:9.2 --build-arg VERSION=9.2-devel-centos7 .
 docker tag osuosl/centos-ppc64le-cuda:9.1 osuosl/centos-ppc64le-cuda:latest
 docker build -t osuosl/centos-ppc64le-cuda:8.0-cudnn6 --build-arg VERSION=8.0-cudnn6-devel-centos7 .
 docker build -t osuosl/centos-ppc64le-cuda:8.0-cudnn7 --build-arg VERSION=8.0-cudnn7-devel-centos7 .
 docker build -t osuosl/centos-ppc64le-cuda:9.0-cudnn7 --build-arg VERSION=9.0-cudnn7-devel-centos7 .
 docker build -t osuosl/centos-ppc64le-cuda:9.1-cudnn7 --build-arg VERSION=9.1-cudnn7-devel-centos7 .
+docker build -t osuosl/centos-ppc64le-cuda:9.2-cudnn7 --build-arg VERSION=9.2-cudnn7-devel-centos7 .
--- ubuntu-ppc64le-cuda/build.sh
+++ ubuntu-ppc64le-cuda/build.sh
@@ -3,8 +3,10 @@ set -ex
 docker build -t osuosl/ubuntu-ppc64le-cuda:8.0 --build-arg VERSION=8.0-devel-ubuntu16.04 .
 docker build -t osuosl/ubuntu-ppc64le-cuda:9.0 --build-arg VERSION=9.0-devel-ubuntu16.04 .
 docker build -t osuosl/ubuntu-ppc64le-cuda:9.1 --build-arg VERSION=9.1-devel-ubuntu16.04 .
+docker build -t osuosl/ubuntu-ppc64le-cuda:9.2 --build-arg VERSION=9.2-devel-ubuntu16.04 .
 docker tag osuosl/ubuntu-ppc64le-cuda:9.1 osuosl/ubuntu-ppc64le-cuda:latest
 docker build -t osuosl/ubuntu-ppc64le-cuda:8.0-cudnn6 --build-arg VERSION=8.0-cudnn6-devel-ubuntu16.04 .
 docker build -t osuosl/ubuntu-ppc64le-cuda:8.0-cudnn7 --build-arg VERSION=8.0-cudnn7-devel-ubuntu16.04 .
 docker build -t osuosl/ubuntu-ppc64le-cuda:9.0-cudnn7 --build-arg VERSION=9.0-cudnn7-devel-ubuntu16.04 .
 docker build -t osuosl/ubuntu-ppc64le-cuda:9.1-cudnn7 --build-arg VERSION=9.1-cudnn7-devel-ubuntu16.04 .
+docker build -t osuosl/ubuntu-ppc64le-cuda:9.2-cudnn7 --build-arg VERSION=9.2-cudnn7-devel-ubuntu16.04 .

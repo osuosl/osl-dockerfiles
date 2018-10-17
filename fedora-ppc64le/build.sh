b319b487f809972a45206260344823901cafc324
Lance Albertson <lance@osuosl.org>
2018-10-17 09:59:43 -0700

    Update Fedora ppc64le images

--- fedora-ppc64le/build.sh
+++ fedora-ppc64le/build.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 set -ex
-docker build -t osuosl/fedora-ppc64le:26 .
-docker build -t osuosl/fedora-ppc64le:27 .
-docker tag osuosl/fedora-ppc64le:27 osuosl/fedora-ppc64le:latest
+docker build -t osuosl/fedora-ppc64le:26 --build-arg VERSION=26 .
+docker build -t osuosl/fedora-ppc64le:27 --build-arg VERSION=27 .
+docker build -t osuosl/fedora-ppc64le:28 --build-arg VERSION=28 .
+docker tag osuosl/fedora-ppc64le:28 osuosl/fedora-ppc64le:latest
--- fedora-ppc64le/push.sh
+++ fedora-ppc64le/push.sh
@@ -2,4 +2,5 @@
 set -ex
 docker push osuosl/fedora-ppc64le:26
 docker push osuosl/fedora-ppc64le:27
+docker push osuosl/fedora-ppc64le:28
 docker push osuosl/fedora-ppc64le:latest

957b144cdfd7d3791e8b8f0564c337693c1f6f55
Lance Albertson <lance@osuosl.org>
2019-01-08 15:51:25 -0800

    Update to CUDA 10.0 for ubuntu

--- ubuntu-ppc64le-cuda/push.sh
+++ ubuntu-ppc64le-cuda/push.sh
@@ -4,9 +4,11 @@ docker push osuosl/ubuntu-ppc64le-cuda:8.0
 docker push osuosl/ubuntu-ppc64le-cuda:9.0
 docker push osuosl/ubuntu-ppc64le-cuda:9.1
 docker push osuosl/ubuntu-ppc64le-cuda:9.2
+docker push osuosl/ubuntu-ppc64le-cuda:10.0
 docker push osuosl/ubuntu-ppc64le-cuda:latest
 docker push osuosl/ubuntu-ppc64le-cuda:8.0-cudnn6
 docker push osuosl/ubuntu-ppc64le-cuda:8.0-cudnn7
 docker push osuosl/ubuntu-ppc64le-cuda:9.0-cudnn7
 docker push osuosl/ubuntu-ppc64le-cuda:9.1-cudnn7
 docker push osuosl/ubuntu-ppc64le-cuda:9.2-cudnn7
+docker push osuosl/ubuntu-ppc64le-cuda:10.0-cudnn7

b6423889b176732bdb1b83f5d21ad6f9dd141185
Lance Albertson <lance@osuosl.org>
2019-04-16 14:25:35 -0700

    Use OpenJDK 11 on buster or unstable

--- debian-ppc64le/build.sh
+++ debian-ppc64le/build.sh
@@ -2,5 +2,5 @@
 set -ex
 docker build -t osuosl/debian-ppc64le:9 --build-arg VERSION=9 .
 docker tag osuosl/debian-ppc64le:9 osuosl/debian-ppc64le:latest
-docker build -t osuosl/debian-ppc64le:buster --build-arg VERSION=buster .
-docker build -t osuosl/debian-ppc64le:unstable --build-arg VERSION=unstable .
+docker build -t osuosl/debian-ppc64le:buster --build-arg VERSION=buster --build-arg JDK_VER=11 .
+docker build -t osuosl/debian-ppc64le:unstable --build-arg VERSION=unstable --build-arg JDK_VER=11 .
--- debian-s390x/build.sh
+++ debian-s390x/build.sh
@@ -2,5 +2,5 @@
 set -ex
 docker build -t osuosl/debian-s390x:9 --build-arg VERSION=9 .
 docker tag osuosl/debian-s390x:9 osuosl/debian-s390x:latest
-docker build -t osuosl/debian-s390x:buster --build-arg VERSION=buster .
-docker build -t osuosl/debian-s390x:unstable --build-arg VERSION=unstable .
+docker build -t osuosl/debian-s390x:buster --build-arg VERSION=buster --build-arg JDK_VER=11 .
+docker build -t osuosl/debian-s390x:unstable --build-arg VERSION=unstable --build-arg JDK_VER=11 .

8e6e4a5a9893f45e99c0c645cf5acbb3e45082be
Lance Albertson <lance@osuosl.org>
2019-04-16 13:30:55 -0700

    Use 18.04 as the latest

--- ubuntu-s390x/build.sh
+++ ubuntu-s390x/build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -ex
 docker build -t osuosl/ubuntu-s390x:16.04 --build-arg VERSION=16.04 .
-docker tag osuosl/ubuntu-s390x:16.04 osuosl/ubuntu-s390x:latest
 docker build -t osuosl/ubuntu-s390x:18.04 --build-arg VERSION=18.04 .
+docker tag osuosl/ubuntu-s390x:18.04 osuosl/ubuntu-s390x:latest

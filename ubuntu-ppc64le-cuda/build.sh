61e2859c9253f59e36829b6de44321068f810356
Lance Albertson <lance@osuosl.org>
2019-04-16 13:30:40 -0700

    Update CUDA versions

--- ubuntu-ppc64le-cuda/build.sh
+++ ubuntu-ppc64le-cuda/build.sh
@@ -5,10 +5,12 @@ docker build -t osuosl/ubuntu-ppc64le-cuda:9.0 --build-arg VERSION=9.0-devel-ubu
 docker build -t osuosl/ubuntu-ppc64le-cuda:9.1 --build-arg VERSION=9.1-devel-ubuntu16.04 .
 docker build -t osuosl/ubuntu-ppc64le-cuda:9.2 --build-arg VERSION=9.2-devel-ubuntu16.04 .
 docker build -t osuosl/ubuntu-ppc64le-cuda:10.0 --build-arg VERSION=10.0-devel-ubuntu18.04 .
-docker tag osuosl/ubuntu-ppc64le-cuda:10.0 osuosl/ubuntu-ppc64le-cuda:latest
+docker build -t osuosl/ubuntu-ppc64le-cuda:10.1 --build-arg VERSION=10.0-devel-ubuntu18.04 .
+docker tag osuosl/ubuntu-ppc64le-cuda:10.1 osuosl/ubuntu-ppc64le-cuda:latest
 docker build -t osuosl/ubuntu-ppc64le-cuda:8.0-cudnn6 --build-arg VERSION=8.0-cudnn6-devel-ubuntu16.04 .
 docker build -t osuosl/ubuntu-ppc64le-cuda:8.0-cudnn7 --build-arg VERSION=8.0-cudnn7-devel-ubuntu16.04 .
 docker build -t osuosl/ubuntu-ppc64le-cuda:9.0-cudnn7 --build-arg VERSION=9.0-cudnn7-devel-ubuntu16.04 .
 docker build -t osuosl/ubuntu-ppc64le-cuda:9.1-cudnn7 --build-arg VERSION=9.1-cudnn7-devel-ubuntu16.04 .
 docker build -t osuosl/ubuntu-ppc64le-cuda:9.2-cudnn7 --build-arg VERSION=9.2-cudnn7-devel-ubuntu16.04 .
 docker build -t osuosl/ubuntu-ppc64le-cuda:10.0-cudnn7 --build-arg VERSION=10.0-cudnn7-devel-ubuntu18.04 .
+docker build -t osuosl/ubuntu-ppc64le-cuda:10.1-cudnn7 --build-arg VERSION=10.1-cudnn7-devel-ubuntu18.04 .
--- ubuntu-ppc64le-cuda/push.sh
+++ ubuntu-ppc64le-cuda/push.sh
@@ -5,6 +5,7 @@ docker push osuosl/ubuntu-ppc64le-cuda:9.0
 docker push osuosl/ubuntu-ppc64le-cuda:9.1
 docker push osuosl/ubuntu-ppc64le-cuda:9.2
 docker push osuosl/ubuntu-ppc64le-cuda:10.0
+docker push osuosl/ubuntu-ppc64le-cuda:10.1
 docker push osuosl/ubuntu-ppc64le-cuda:latest
 docker push osuosl/ubuntu-ppc64le-cuda:8.0-cudnn6
 docker push osuosl/ubuntu-ppc64le-cuda:8.0-cudnn7
@@ -12,3 +13,4 @@ docker push osuosl/ubuntu-ppc64le-cuda:9.0-cudnn7
 docker push osuosl/ubuntu-ppc64le-cuda:9.1-cudnn7
 docker push osuosl/ubuntu-ppc64le-cuda:9.2-cudnn7
 docker push osuosl/ubuntu-ppc64le-cuda:10.0-cudnn7
+docker push osuosl/ubuntu-ppc64le-cuda:10.1-cudnn7

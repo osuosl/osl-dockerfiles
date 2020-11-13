a829471704b031992c5223f6aa3eceb8ba1fe82b
Lance Albertson <lance@osuosl.org>
2020-11-13 12:05:41 -0800

    Add date timestamp

--- etherpad-debian/build-snowdrift.sh
+++ etherpad-debian/build-snowdrift.sh
@@ -2,3 +2,5 @@
 set -ex
 ETHERPAD_PLUGINS="ep_adminpads2 ep_chatdate ep_desktop_notifications ep_hide_referrer ep_small_list ep_comments_page ep_author_hover ep_webrtc"
 docker build . --build-arg ETHERPAD_PLUGINS="$ETHERPAD_PLUGINS" --tag osuosl/etherpad-snowdrift
+DATE_VER="$(date -u +%Y.%m.%d.%H%M)"
+docker tag osuosl/etherpad osuosl/etherpad-snowdrift:1.8.6-${DATE_VER}
--- etherpad-debian/build.sh
+++ etherpad-debian/build.sh
@@ -2,3 +2,5 @@
 set -ex
 ETHERPAD_PLUGINS=""
 docker build . --build-arg ETHERPAD_PLUGINS="$ETHERPAD_PLUGINS" --tag osuosl/etherpad
+DATE_VER="$(date -u +%Y.%m.%d.%H%M)"
+docker tag osuosl/etherpad osuosl/etherpad:1.8.6-${DATE_VER}

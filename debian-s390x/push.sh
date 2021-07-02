129a06e08c074eda9b566a6b7d89e4ffdae714d1
Lance Albertson <lance@osuosl.org>
2021-07-02 09:09:23 -0700

    Fix typo
    Signed-off-by: Lance Albertson <lance@osuosl.org>

--- debian-s390x/push.sh
+++ debian-s390x/push.sh
@@ -3,5 +3,5 @@ set -ex
 docker push osuosl/debian-s390x:9
 docker push osuosl/debian-s390x:10
 docker push osuosl/debian-s390x:latest
-docker push osuosl/debian-s390x:bulleye
+docker push osuosl/debian-s390x:bullseye
 docker push osuosl/debian-s390x:unstable

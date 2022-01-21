c14dcb0b10d3a28ae3e4e5051b049ad96d087058
Lance Albertson <lance@osuosl.org>
2022-01-21 09:35:35 -0800

    Update to Redmine 4.2.3
    Signed-off-by: Lance Albertson <lance@osuosl.org>

--- redmine-replicant/build.sh
+++ redmine-replicant/build.sh
@@ -2,4 +2,4 @@
 set -ex
 docker build . --tag osuosl/redmine-replicant
 DATE_VER="$(date -u +%Y.%m.%d.%H%M)"
-docker tag osuosl/redmine-replicant osuosl/redmine-replicant:4.1.1-${DATE_VER}
+docker tag osuosl/redmine-replicant osuosl/redmine-replicant:4.2.3-${DATE_VER}

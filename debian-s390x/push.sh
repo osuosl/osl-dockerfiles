47dae4b9ae38de73d41a97937aa105c31a03eb80
Lance Albertson <lance@osuosl.org>
2024-09-23 09:08:31 -0700

    Additional updates
    Signed-off-by: Lance Albertson <lance@osuosl.org>

--- debian-s390x/push.sh
+++ debian-s390x/push.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 set -ex
-docker push osuosl/debian-s390x:9
-docker push osuosl/debian-s390x:10
+docker push osuosl/debian-s390x:12
 docker push osuosl/debian-s390x:latest
-docker push osuosl/debian-s390x:bullseye
-docker push osuosl/debian-s390x:unstable
